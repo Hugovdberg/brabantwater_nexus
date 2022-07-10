5b19f6c0123c36cf7406d1bac1d150f1be6aeed0
Hugo Lapré <hugo.lapre@brabantwater.nl>
2022-07-10 13:14:07 +0200

    Create an enumeration of monitoring station owners

---
+++ src/brabantwater_nexus/owner.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class Owner(enum.Enum):
+    """Eigenaar van een meetpunt"""
+
+    BRABANT_WATER = "Brabant Water"
+    GEM_GOIRLE = "Gem. Goirle"
+    GEM_TILBURG = "Gem. Tilburg"
+    LANDGOED_DE_HOEVENS = "Lgd. De Hoevens"
+    PROVINCIE = "Prov. Noord-Brabant"
+    WS_DE_DOMMEL = "WS De Dommel"
+    WS_BRABANTSE_DELTA = "WS Brabantse Delta"
