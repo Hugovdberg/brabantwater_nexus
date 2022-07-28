370656d12f9f7eee32f7049de9dd5556d70585ca
Hugo Lapré <hugo.lapre@brabantwater.nl>
2022-07-28 16:13:35 +0200

    add groundwater quality parameters

--- src/brabantwater_nexus/timeseries_parameter.py
+++ src/brabantwater_nexus/timeseries_parameter.py
@@ -4,3 +4,8 @@ import enum
 class TimeseriesParameter(enum.Enum):
     GROUNDWATER_HEAD = "groundwater_head_m_msl"
     GROUNDWATER_HEAD_MANUAL = "groundwater_head_manual_m_msl"
+    COLLECTOR_REMARK = "collector_remark"
+    PH_FIELD = "groundwater_ph_field"
+    ELECTRIC_CONDUCTIVITY_FIELD = "groundwater_ec_us_field_cm"
+    PH = "groundwater_ph"
+    ELECTRIC_CONDUCTIVITY = "groundwater_ec_us_cm"
